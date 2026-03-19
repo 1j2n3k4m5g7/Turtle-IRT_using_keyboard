f8b9973063b3542189e6ae808f04b22452e031b6
1j2n3k4m5g7 <1j2n3k4m5g7dev@gmail.com>
2026-03-19 18:12:31 +0100

    Update save.py

--- customlibs/save.py
+++ customlibs/save.py
@@ -2,8 +2,7 @@
 # © 2026 Jan Migo
 # Licensed under CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
 # Additional permission: Monetized streaming content allowed (YouTube, Twitch, TikTok, Instagram, etc.)
-# Paid apps/games/software require separate permission
-# Third-party code: Ghostscript (AGPL-3.0) if used
+# Paid apps/games/software require separate permission
 
 from turtle import *
 
